f87d86a92ec7ddba321e4d6f8ace7955d75f5e80
Brook.Yang <18861193929@163.com>
2025-02-03 22:44:15 +0800

    delete foundfry

--- brook-stake/SmartContract/test/BrookStake.t.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-import {BrookStake} from "../contracts/BrookStake.sol";
-// import { Test } from "forge-std/Test.sol';
-
-// contract BrookStakeTest is Test {
-
-// }
